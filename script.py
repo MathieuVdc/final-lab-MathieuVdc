a812a89effa9fb854bdddebbb9516d5dfae6aceb
mvandecasteele <mathieu.vandecasteele@insa-rouen.fr>
2018-12-20 22:36:04 +0100

    Functions for Tokenize
    Signed-off-by: mvandecasteele <mathieu.vandecasteele@insa-rouen.fr>

--- script.py
+++ script.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import Counter
+from sklearn.feature_extraction.text import CountVectorizer
 import seaborn as sns
 import pandas as pd
 import os
+import string
 
 def __count_words_in_a_file(file_path):
     
@@ -11,10 +13,25 @@ def __count_words_in_a_file(file_path):
     return len(Counter(file.read().split()))
 
 
+def __get_lines(file_path):
+    lines = open(file_path).readlines()
+    newlines = []
+    table = str.maketrans('', '', string.punctuation+'’°“‘”—»«®©℠™')
+    for line in lines :
+        clean = line.rstrip()
+        clean_better = (clean.translate(table)).lower()
+        clean_better_better = clean_better.strip()
+        if (clean_better_better != ''):
+            newlines.append(clean_better_better)
+    return newlines
+
+
 def __list_words_in_a_file(file_path):
     
-    file = open(file_path, "r", encoding="utf-8")
-    return Counter(file.read().split())
+    lines = __get_lines(file_path)
+    count_vec = CountVectorizer()
+    vector = count_vec.fit_transform(lines)
+    return count_vec.get_feature_names()
 
 
 def count_average_number_of_words_per_class(folder_path):
@@ -32,11 +49,14 @@ def list_of_words_per_class(folder_path):
     
     results = {}
     for directory in os.listdir(folder_path):
+        print(directory)
         list_words = []
         for textfile in os.listdir(os.path.join(folder_path,directory)):
+            print(textfile)
             list_words.extend(__list_words_in_a_file(os.path.join(folder_path,directory,textfile)))
         results[directory] = list_words
     return results
+
 
 def most_popular_elements_in_list(list_to_top, number_wanted):
 

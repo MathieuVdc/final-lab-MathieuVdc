a743e8fa50c136c94058012ed0b13196a2b27f1a
mvandecasteele <mathieu.vandecasteele@insa-rouen.fr>
2018-12-20 23:18:20 +0100

    Tokenizing works perfectly now Signed-off-by: mvandecasteele <mathieu.vandecasteele@insa-rouen.fr>

--- script.py
+++ script.py
@@ -6,32 +6,14 @@ import seaborn as sns
 import pandas as pd
 import os
 import string
+from sklearn import preprocessing
+
+
 
 def __count_words_in_a_file(file_path):
     
     file = open(file_path, "r", encoding="utf-8")
     return len(Counter(file.read().split()))
-
-
-def __get_lines(file_path):
-    lines = open(file_path).readlines()
-    newlines = []
-    table = str.maketrans('', '', string.punctuation+'’°“‘”—»«®©℠™')
-    for line in lines :
-        clean = line.rstrip()
-        clean_better = (clean.translate(table)).lower()
-        clean_better_better = clean_better.strip()
-        if (clean_better_better != ''):
-            newlines.append(clean_better_better)
-    return newlines
-
-
-def __list_words_in_a_file(file_path):
-    
-    lines = __get_lines(file_path)
-    count_vec = CountVectorizer()
-    vector = count_vec.fit_transform(lines)
-    return count_vec.get_feature_names()
 
 
 def count_average_number_of_words_per_class(folder_path):
@@ -45,23 +27,34 @@ def count_average_number_of_words_per_class(folder_path):
     return results
 
 
-def list_of_words_per_class(folder_path):
+def __get_lines_cleaned(file_path):
+    lines = open(file_path).readlines()
+    newlines = []
+    table = str.maketrans('', '', string.punctuation+'’°“‘”—»«®©℠™')
+    for line in lines :
+        clean = line.rstrip()
+        clean_better = (clean.translate(table)).lower()
+        clean_better_better = clean_better.strip()
+        if (clean_better_better != ''):
+            newlines.append(clean_better_better)
+    return newlines
+
+
+def data_pre_processing(input_csv_path):
+   
+    raw_data = pd.read_csv(input_csv_path, sep = ",")
+    x = []
+    y = []
+    for index, row in raw_data.iterrows():
+        ocr_path = row['img_path'].replace('.jpg','.txt')
+        x.append(__get_lines_cleaned(os.path.join('tobacco-lab/data/Tobacco3482-OCR/'+ ocr_path)))
+        y.append(row['label'])
+    labelencod = preprocessing.LabelEncoder()
+    y_num = list(labelencod.fit_transform(y))
+    return [x,y]
+
+
     
-    results = {}
-    for directory in os.listdir(folder_path):
-        print(directory)
-        list_words = []
-        for textfile in os.listdir(os.path.join(folder_path,directory)):
-            print(textfile)
-            list_words.extend(__list_words_in_a_file(os.path.join(folder_path,directory,textfile)))
-        results[directory] = list_words
-    return results
 
-
-def most_popular_elements_in_list(list_to_top, number_wanted):
-
-    return dict(Counter(list_to_top).most_common(number_wanted))
-
-            
             
     
